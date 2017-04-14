6c3c4f7ce1addfe8f10532a02f75c10a85db1b3e
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-04-14 16:51:39 +0800

    changes made

--- NewChatApp/ProfileViewController.swift
+++ NewChatApp/ProfileViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseAuth
+import FirebaseStorage
 
 class ProfileViewController: UIViewController {
     
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var statusTextField: UITextField!
+    @IBOutlet weak var phoneNumTextField: UITextField!
     //var profileArray: [[String:Any]] = [["name":"\()"]]
     
     override func viewDidLoad() {
